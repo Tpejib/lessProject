ff9bf9689b63fa7685b805da112b0cd109ab2103
Vitaliy <tpejib@gmail.com>
2020-02-05 17:36:04 +0200

    less 9

---
+++ yoga/js/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tab.length; i++) {
+            tabContent[i].classList.add('hide');
+            tabContent[i].classList.remove('show');
+        }
+    }
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                }
+            }
+        }
+    });
+});
